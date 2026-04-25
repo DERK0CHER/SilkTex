a71bc84dd71146c6dff6c2be19cc598f3311d170
DERK0CHER <andenmaus@outlook.com>
2026-04-25 23:48:13 +0200

    Delete .opam-goblint directory

--- .opam-goblint/opam-init/init.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-  test -r '/Users/beba/software/SilkTex/.opam-goblint/opam-init/env_hook.fish' && source '/Users/beba/software/SilkTex/.opam-goblint/opam-init/env_hook.fish' > /dev/null 2> /dev/null; or true
-end
-
-test -r '/Users/beba/software/SilkTex/.opam-goblint/opam-init/variables.fish' && source '/Users/beba/software/SilkTex/.opam-goblint/opam-init/variables.fish' > /dev/null 2> /dev/null; or true
